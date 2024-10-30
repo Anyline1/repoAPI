01d3dd9343a0340242855b96d0a689fc8e512d1f
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-10-30 20:21:25 +0300

    add exceptions

---
+++ src/main/java/ru/anyline/repoapi/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package ru.anyline.repoapi.exceptions;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserReposException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleUserReposException(UserReposException e, Model model) {
+        model.addAttribute("error", e.getMessage());
+        return "error"; 
+    }
+}
+
