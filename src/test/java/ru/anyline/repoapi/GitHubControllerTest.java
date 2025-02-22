b039a8979e4d65d749e1db5f5133454cb885271e
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-02-22 19:01:15 +0300

    fix

--- src/test/java/ru/anyline/repoapi/GitHubControllerTest.java
+++ src/test/java/ru/anyline/repoapi/GitHubControllerTest.java
@@ -15,8 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 public class GitHubControllerTest {
