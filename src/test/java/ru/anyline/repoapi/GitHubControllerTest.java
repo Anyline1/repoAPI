97e9ff7d4a59e61354ca4eaad15d5e93d4e8c7ab
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-02-26 13:03:27 +0300

    fix

--- src/test/java/ru/anyline/repoapi/GitHubControllerTest.java
+++ src/test/java/ru/anyline/repoapi/GitHubControllerTest.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
