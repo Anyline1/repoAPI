5872248c30b7b03cad64e3fc0f17832813e53e8b
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-02-28 12:34:53 +0300

    test

---
+++ src/test/java/ru/anyline/repoapi/LoginControllerTest.java
@@ -0,0 +1,9 @@
+package ru.anyline.repoapi;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import ru.anyline.repoapi.controller.LoginController;
+
+@WebMvcTest(LoginController.class)
+class LoginControllerTest {
+
+}
