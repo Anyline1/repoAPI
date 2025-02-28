a2f6f59c80a2b8c3903a9a184cb6d600050578c9
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-02-28 12:35:59 +0300

    fix

--- src/test/java/ru/anyline/repoapi/LoginControllerTest.java
+++ src/test/java/ru/anyline/repoapi/LoginControllerTest.java
@@ -1,7 +1,9 @@
 package ru.anyline.repoapi;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.anyline.repoapi.controller.LoginController;
 
 @WebMvcTest(LoginController.class)
