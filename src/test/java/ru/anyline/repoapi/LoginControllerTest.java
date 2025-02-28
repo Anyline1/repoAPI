afc4b7dfd1ad913888e008cc9dd6b007a4035003
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-02-28 12:35:37 +0300

    test

--- src/test/java/ru/anyline/repoapi/LoginControllerTest.java
+++ src/test/java/ru/anyline/repoapi/LoginControllerTest.java
@@ -5,5 +5,13 @@ import ru.anyline.repoapi.controller.LoginController;
 
 @WebMvcTest(LoginController.class)
 class LoginControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void shouldReturnLoginPageWhenGetRequestIsMadeToLoginEndpoint() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
 }
