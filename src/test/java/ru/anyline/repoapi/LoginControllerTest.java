570b19c314807c715975198b6bb7b615179a1bf6
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-03-02 21:36:50 +0300

    fix

--- src/test/java/ru/anyline/repoapi/LoginControllerTest.java
+++ src/test/java/ru/anyline/repoapi/LoginControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.anyline.repoapi.controller.LoginController;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
