de455123fd7489c6d0c9920c799c1ab2759547f5
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-03-07 20:27:41 +0300

    test

---
+++ src/test/java/ru/anyline/repoapi/UserReposControllerTest.java
@@ -0,0 +1,15 @@
+package ru.anyline.repoapi;
+
+import org.springframework.ui.Model;
+import org.springframework.web.client.RestTemplate;
+import ru.anyline.repoapi.controller.UserReposController;
+
+class UserReposControllerTest {
+
+    private RestTemplate restTemplate;
+
+    private Model model;
+
+    private UserReposController userReposController;
+
+}
