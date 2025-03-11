e1f7b101ff015654b3c6c649856128dea04d135d
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-03-11 16:52:41 +0300

    test

--- src/test/java/ru/anyline/repoapi/UserReposControllerTest.java
+++ src/test/java/ru/anyline/repoapi/UserReposControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import ru.anyline.repoapi.controller.UserReposController;
 import ru.anyline.repoapi.model.UserRepos;
