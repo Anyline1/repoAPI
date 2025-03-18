3a85dd01385ea7f6ceee5b6a4488218ab7ed189c
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-03-18 18:57:06 +0300

    repo

--- src/main/java/ru/anyline/repoapi/repository/UserProjectRepository.java
+++ src/main/java/ru/anyline/repoapi/repository/UserProjectRepository.java
@@ -3,5 +3,8 @@ package ru.anyline.repoapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.anyline.repoapi.model.UserProject;
 
+import java.util.List;
+
 public interface UserProjectRepository extends JpaRepository<UserProject, String> {
+    List<UserProject> findByUserId(Long userId);
 }
