5b1b69f6bf0680c651ba4f5339ab22c59e14f224
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-03-18 19:06:21 +0300

    repo

--- src/main/java/ru/anyline/repoapi/repository/UserProjectRepository.java
+++ src/main/java/ru/anyline/repoapi/repository/UserProjectRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import ru.anyline.repoapi.model.UserProject;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserProjectRepository extends JpaRepository<UserProject, String> {
@@ -17,5 +18,5 @@ public interface UserProjectRepository extends JpaRepository<UserProject, String
     void deleteById(Long id);
 
     Optional<UserProject> findById(String id);
-    
+
 }
