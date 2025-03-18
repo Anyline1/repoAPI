945e30e361fac3ebf488c772c6ba676d2c033d90
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-03-18 18:59:33 +0300

    entity

--- src/main/java/ru/anyline/repoapi/model/UserProject.java
+++ src/main/java/ru/anyline/repoapi/model/UserProject.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 
 @Entity
+@Data
 public class UserProject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,5 +21,4 @@ public class UserProject {
         return null;
     }
 
-    // Getters and setters
 }
