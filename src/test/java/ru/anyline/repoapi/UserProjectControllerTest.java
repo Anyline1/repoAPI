2b56ea70b10a01d313de165ac3757a5ed8cf52c1
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-03-27 22:20:08 +0300

    fix

--- src/test/java/ru/anyline/repoapi/UserProjectControllerTest.java
+++ src/test/java/ru/anyline/repoapi/UserProjectControllerTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.any;
 
 class UserProjectControllerTest {
 
