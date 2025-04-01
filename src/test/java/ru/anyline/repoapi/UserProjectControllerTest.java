62459e5bb974586fe29beae7a92b261a7d005d01
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-04-01 13:48:04 +0300

    fix

--- src/test/java/ru/anyline/repoapi/UserProjectControllerTest.java
+++ src/test/java/ru/anyline/repoapi/UserProjectControllerTest.java
@@ -9,6 +9,7 @@ import ru.anyline.repoapi.controller.UserProjectController;
 import ru.anyline.repoapi.model.UserProject;
 import ru.anyline.repoapi.service.UserProjectServiceImpl;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
