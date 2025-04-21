eca6dbeb15922b72b184b43b1c4046282b8bccef
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-04-21 19:13:06 +0300

    fix

--- src/main/java/ru/anyline/repoapi/exceptions/ProjectNotFoundException.java
+++ src/main/java/ru/anyline/repoapi/exceptions/ProjectNotFoundException.java
@@ -1,4 +1,8 @@
 package ru.anyline.repoapi.exceptions;
 
 public class ProjectNotFoundException extends RuntimeException{
+
+    public ProjectNotFoundException(String message){
+        super(message);
+    }
 }
