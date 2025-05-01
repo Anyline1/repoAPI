2df326e6a2057c633f5610befa5a60d7e51a8ed8
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-05-01 19:34:41 +0300

    fix

--- src/test/java/ru/anyline/repoapi/UserProjectControllerTest.java
+++ src/test/java/ru/anyline/repoapi/UserProjectControllerTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
